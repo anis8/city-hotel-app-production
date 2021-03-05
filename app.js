4d658b4c742de9e2c5841d1736917063ea88e228
Cold <51854565+Cold-FR@users.noreply.github.com>
2021-03-05 22:50:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ try {
             mainWindow = null;
         });
 
-        mainWindow.webContents.openDevTools();
+        ///mainWindow.webContents.openDevTools();
 
         await mainWindow.loadURL(url.format({
             pathname: path.join(__dirname, `app.html`),
